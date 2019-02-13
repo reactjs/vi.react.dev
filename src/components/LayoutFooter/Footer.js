36c7c1234e9ddad7e0106a03ddb66de470d532d0
nampdn <nampdn@protonmail.com>
2019-02-13 08:57:55 +0700

    style(Footer): fix prettier format

--- src/components/LayoutFooter/Footer.js
+++ src/components/LayoutFooter/Footer.js
@@ -131,9 +131,7 @@ const Footer = ({layoutHasSidebar = false}: {layoutHasSidebar: boolean}) => (
             <MetaTitle onDark={true}>Xem Thêm</MetaTitle>
             <FooterLink to="/tutorial/tutorial.html">Hướng Dẫn</FooterLink>
             <FooterLink to="/blog/">Blog</FooterLink>
-            <FooterLink to="/acknowledgements.html">
-              Lời Cảm Ơn
-            </FooterLink>
+            <FooterLink to="/acknowledgements.html">Lời Cảm Ơn</FooterLink>
             <ExternalFooterLink
               href="https://facebook.github.io/react-native/"
               target="_blank"
